4427e096e1383e3e61012cda60335b3fc50f60fd
Eduardo Arnold <eduardo.arnold@outlook.com>
2015-03-24 23:56:30 -0300

    chore(Gruntfile): configura uglify e connect

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    uglify: {
+      options: {
+        mangle: false,
+      },
+      my_target: {
+        options: {
+          sourceMap: true,
+          sourceMapName: 'app/public/dist/js/bundle.min.map'
+        },
+        files: {
+          'app/public/dist/js/bundle.min.js': [
+            'app/public/assets/libs/angular/angular.min.js', 
+            'app/public/app.module.js',
+            'app/public/shared/**/*.js',
+            'app/public/sections/**/*.js'
+          ]
+        }
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          base: 'app/public/',
+          port: 9000,
+          hostname: 'localhost',
+          keepalive: true,
+          protocol: 'http'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask('s', ['uglify', 'connect']);
+}
