e5d6ad7acc5148dd19ec9cc9b7f202e620591a10
DuduArnold <eduardo.arnold@outlook.com>
2015-03-27 00:07:23 -0300

    feat(mongoose)Configuração de conexão com o banco

--- app/server/server.js
+++ app/server/server.js
@@ -1,15 +1,30 @@
+// Environment & Configuration Variables
+var environment = process.env.NODE_ENV;
+var databaseURI = process.env.CONN_STRING;
+
+// Set Up
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
+var mongoose = require('mongoose');
 
 app.use(express.static(path.resolve('app/public/')));
 
-// GET: /
+// Database Connection
+var connectionString = "mongodb://" + databaseURI;
+mongoose.connect(connectionString);
+
+
+
+// Server Initialization
 app.get('*', function(req, res) {
     res.sendFile(path.resolve('app/public/index.html'));
 });
 
 var port = process.env.PORT || 9000;
 
-app.listen(port);
+app.listen(port, function() {
+    console.log("App listening as %s on port %d", app.settings.env, this.address().port);
+    console.log("Connected to database on: " + connectionString);
+});
