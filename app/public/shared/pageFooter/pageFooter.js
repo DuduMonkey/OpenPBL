cfc10466cc8227f0d29294338d80eac5039e51a9
Eduardo Arnold <eduardo.arnold@outlook.com>
2015-03-28 15:47:45 -0300

    feat(footer): adiciona footer

--- app/public/shared/pageFooter/pageFooter.js
+++ app/public/shared/pageFooter/pageFooter.js
@@ -1,10 +1,10 @@
-app.directive('pageFooter', function () {
-  return {
-    restrict: 'E',
-    replace: true,
-    templateUrl: '/shared/pageFooter/pageFooter.html',
-    link: function () {
-      console.log('uou');
+angular.module('openpbl.directives')
+  .directive('pageFooter', function () {
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: '/shared/pageFooter/pageFooter.tpl.html',
+      link: function () {
+      }
     }
-  }
-});
+  });
