7c7c2a7db84c84b0e8f6f57e9ae2d6f39c7ffaa4
Eduardo Arnold <eduardo.arnold@outlook.com>
2015-03-28 15:47:29 -0300

    feat(header): adiciona header

--- app/public/shared/pageHeader/pageHeader.js
+++ app/public/shared/pageHeader/pageHeader.js
@@ -1,7 +1,11 @@
-app.directive('pageHeader', function () {
-  return {
-    restrict: 'E',
-    replace: true,
-    templateUrl: '/shared/pageHeader/pageHeader.html'
-  }
-});
+angular.module('openpbl.directives')
+  .directive('pageHeader', function () {
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: '/shared/pageHeader/pageHeader.tpl.html',
+      link: function () {
+        
+      }
+    }
+  });
