407fd382adfbd841edae3a103d2f1ea60d1cb072
Eduardo Arnold <eduardo.arnold@outlook.com>
2015-03-27 23:55:27 -0300

    style(server): ajusta identação

--- app/server/server.js
+++ app/server/server.js
@@ -1,13 +1,21 @@
+'use strict';
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
 
-app.use(express.static(path.resolve('app/public/')));
+app.use(express.static(path.resolve('app/public')));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // GET: /
 app.get('*', function(req, res) {
-    res.sendFile(path.resolve('app/public/index.html'));
+  res.sendFile(path.resolve('app/public/index.html'));
 });
 
 var port = process.env.PORT || 9000;
