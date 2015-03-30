4b0a4575eb8975caf02d0a966260d93f31687dec
Eduardo Arnold <eduardo.arnold@outlook.com>
2015-03-30 17:36:35 -0300

    feat(server): Define as sessions

--- app/server/server.js
+++ app/server/server.js
@@ -3,21 +3,41 @@
 // Environment & Configuration Variables
 var environment = process.env.NODE_ENV;
 var databaseURI = process.env.CONN_STRING;
+var sessionSecret = process.env.SESSION_SECRET;
 
 // Set Up
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
 var path = require('path');
+
 var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
 app.use(express.static(path.resolve('app/public')));
 
-app.use(bodyParser.json());
-
+app.use(morgan('dev')); //log requests to console
+app.use(cookieParser()); //read cookies (required for auth)
+app.use(bodyParser.json()); //get information from html forms
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+app.use(session({ 
+  secret: sessionSecret, // session secret
+  saveUninitialized: true,
+  resave: false
+})); 
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
 
 // Database Connection
 var connectionString = "mongodb://" + databaseURI;
