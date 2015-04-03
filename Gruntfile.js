dc1a281c0ca79bb1debe54423f7bfab6445f06a0
Eduardo Arnold <eduardo.arnold@outlook.com>
2015-04-02 21:14:03 -0300

    chore(jshint): adiciona jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,60 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+
+    auto_install: {
+      local: {},
+      subdir: {
+        options: {
+          cwd: 'subdir',
+          stdout: true,
+          stderr: true,
+          failOnError: true,
+          npm: true,
+          bower: true
+        }
+      }
+    },
+
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1,
+        sourceMap: true,
+        report: 'min'
+      },
+      target: {
+        files: {
+          'app/public/dist/css/bundle.min.css': [
+            'app/public/assets/libs/bootstrap/dist/css/bootstrap.min.css',
+            'app/public/assets/css/**/*.css'
+            ]
+        }
+      }
+    },
+
+    express: {
+      options: {
+        background: true,
+        port: 9000,
+        debug: true
+      },
+      dev: {
+        options: {
+          script: 'app/server/server.js'
+        }
+      }
+    },
+
+    jshint: {
+      all: [
+      'Gruntfile.js',
+      'app/public/*.js',
+      'app/public/sections/**/*.js',
+      'app/public/shared/**/*.js'
+      ]
+    },
+
     uglify: {
       options: {
         mangle: false,
@@ -22,67 +76,20 @@ module.exports = function(grunt) {
             ]
         }
       }
-    },
-    cssmin: {
-      options: {
-        shorthandCompacting: false,
-        roundingPrecision: -1,
-        sourceMap: true,
-        report: 'min'
-      },
-      target: {
-        files: {
-          'app/public/dist/css/bundle.min.css': [
-            'app/public/assets/libs/bootstrap/dist/css/bootstrap.min.css',
-            'app/public/assets/css/**/*.css'
-            ]
-        }
-      }
-    },
-    connect: {
-      dev: {
-        options: {
-          base: 'app/public/',
-          livereload: true,
-          port: 9000,
-          hostname: 'localhost',
-          keepalive: true,
-          protocol: 'http'
-        }
-      }
-    },
-    express: {
-      options: {
-        background: true,
-        port: 9000,
-        debug: true
-      },
-      dev: {
-        options: {
-          script: 'app/server/server.js'
-        }
-      }
-    },
-    auto_install: {
-    local: {},
-      subdir: {
-        options: {
-          cwd: 'subdir',
-          stdout: true,
-          stderr: true,
-          failOnError: true,
-          npm: true,
-          bower: true
-        }
-      }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-express-server');
   grunt.loadNpmTasks('grunt-auto-install');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-express-server');
 
-  grunt.registerTask('s', ['auto_install', 'uglify:dev', 'cssmin', 'express:dev']);
+  grunt.registerTask('s', [
+    'auto_install',
+    'jshint',
+    'uglify:dev',
+    'cssmin',
+    'express:dev'
+    ]);
 }
