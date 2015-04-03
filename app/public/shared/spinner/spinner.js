ddb47fbecdd9d0186ea53477644b18f89cd1a912
Eduardo Arnold <eduardo.arnold@outlook.com>
2015-04-02 21:05:04 -0300

    feat(spinner): adiciona spinner
    O spinner é acionado sempre que uma requisiçâo HTTP for feita.

---
+++ app/public/shared/spinner/spinner.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('openpbl.directives')
+  .directive('pblSpinner', function () {
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: '/shared/spinner/spinner.tpl.html'
+    }
+  });
