87d5f12cd644db96e7926fdcdfd36f42a6bd311d
DuduArnold <eduardo.arnold@outlook.com>
2015-04-04 19:22:08 -0300

    feat(Autenticação): Cria o provedor de tokens para usuários

---
+++ app/server/token/TokenProvider.js
@@ -0,0 +1,46 @@
+var Token = require('./Token');
+var Q = require ('Q');
+
+// Constructor
+function TokenProvider() {
+
+
+
+};
+
+// Class Methods
+TokenProvider.prototype.createToken = function(email) {
+
+  var deferred = Q.defer();
+
+  var tokenHash = Token.generateHash();
+
+
+  var newToken = new Token({
+    email : email,
+    token : token
+  });
+
+  newToken.save(function(error, token){
+
+    if(error){
+
+      var errorMessage = {message: 'Erro na criação ao gerar token de sessão'}
+      deferred.reject(errorMessage);
+
+    }else{
+
+      deferred.resolve(token);
+
+    };
+
+  });
+
+  return deferred.promise;
+  
+};
+
+
+
+// export the class
+module.exports = TokenProvider;
