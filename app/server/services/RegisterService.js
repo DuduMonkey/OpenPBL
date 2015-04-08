d0c0315c95e1173d4e47a5b26077007ac452c38d
DuduArnold <eduardo.arnold@outlook.com>
2015-04-07 22:31:02 -0300

    fix(Formatação): Melhorias na formatação da service de registro

--- app/server/services/RegisterService.js
+++ app/server/services/RegisterService.js
@@ -14,12 +14,10 @@ function RegisterService() {
 
     User.find(query).exec(function(error,users){
       if(error){
-
         var errorMessage = 'Erro na busca de usuário';
         deferred.reject(errorMessage);
 
       }else if(users.length > 0){
-
         var errorMessage = 'Email já cadastrado';
         deferred.reject(errorMessage);
 
@@ -42,35 +40,31 @@ RegisterService.prototype.registerUser = function(userData, callback) {
   var newMail = userData.email;
 
   this.isNewValidUser(newMail)
-    .then(function () {
-
-      var newUser = new User({
-        name : userData.name,
-        role : userData.role,
-        email : userData.email
-      });
-
-      newUser.saveHashPassword(userData.password);
-
-      newUser.save(function(error,data){
-
-        if (error) {
-
-          var errorMessage = 'Erro na persistencia dos dados';
-          deferred.reject(errorMessage);
-
-        } else {
-
-          deferred.resolve(data);    
-
-        };
-      });
-    })
-    .catch(function (error) {
-
-      deferred.reject(error);
-
+  .then(function () {
+    var newUser = new User({
+      name : userData.name,
+      role : userData.role,
+      email : userData.email
     });
+
+    newUser.saveHashPassword(userData.password);
+
+    newUser.save(function(error,data){
+
+      if (error) {
+        var errorMessage = 'Erro na persistencia dos dados';
+        deferred.reject(errorMessage);
+
+      } else {
+        deferred.resolve(data);    
+
+      };
+    });
+  })
+  .catch(function (error) {
+    deferred.reject(error);
+
+  });
 
   return deferred.promise;
 
