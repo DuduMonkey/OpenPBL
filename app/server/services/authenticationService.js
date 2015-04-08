c402be63c2116b8424abf29b6d3cb498ff48e02c
DuduArnold <eduardo.arnold@outlook.com>
2015-04-07 22:31:48 -0300

    feat(Autenticação): Cria o serviço de autenticação de usuários

--- app/server/services/authenticationService.js
+++ app/server/services/authenticationService.js
@@ -1,3 +1,64 @@
-exports.mock = function(){
-  return true;
+var User = require('../models/User');
+var Token = require('../token/TokenProvider');
+var Q = require ('Q');
+
+
+AuthenticationService.prototype.loginUser = function(userMail, userPassword){
+
+  var deferred = Q.defer();
+
+  this.verifyUserCredentials(userMail, userPassword)
+  .then(function(){
+
+    Token.createToken(email)
+      .then(function(validToken){
+        deferred.resolve(validToken);
+
+      })
+      .catch(function(error){
+        deferred.reject(error);
+      });
+
+  })
+  .catch(function(error){
+    deferred.reject(error);
+
+  });
+
+  return deferred.promise;
+};
+
+
+function AuthenticationService() {
+
+  this.verifyUserCredentials = function(userMail, userPassword){
+  var deferred = Q.defer();
+
+    var query = {email : userMail};
+
+    User.findOne(query).exec(function(error, user){
+      
+      if(error){
+        var errorMessage = 'Erro na busca de usuário';
+        deferred.reject(errorMessage);
+
+      }else if(!user){
+        var errorMessage = 'Usuário inexistente';
+        deferred.reject(errorMessage);
+      }
+
+      var passwordIsValid = user.validatePassword(userPassword);
+
+      if(passwordIsValid){
+        deferred.resolve();
+
+      }else{
+        var errorMessage = 'Senha inválida';
+        deferred.reject(errorMessage);
+
+      }
+    });
+
+    return deferred.promise;
+  };
 }
