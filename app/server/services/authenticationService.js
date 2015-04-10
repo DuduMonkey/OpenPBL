c58427cdaae3f691eca9a872601355d15a4e8fc7
DuduArnold <eduardo.arnold@outlook.com>
2015-04-09 23:50:17 -0300

    fix(Autenticação): Aplica as promises para senha na validação de usuário

--- app/server/services/authenticationService.js
+++ app/server/services/authenticationService.js
@@ -31,31 +31,44 @@ AuthenticationService.prototype.authenticateUser = function(userMail, userPasswo
 
 function AuthenticationService() {
 
-  this.verifyUserCredentials = function(userMail, userPassword){
-  var deferred = Q.defer();
+  this.verifyUserCredentials = function(userMail, candidatePassword){
+    var deferred = Q.defer();
 
     var query = {email : userMail};
 
     User.findOne(query).exec(function(error, user){
       if(error){
+
         var errorMessage = 'Erro na busca de usuário';
         deferred.reject(errorMessage);
 
       }else if(!user){
+
         var errorMessage = 'Usuário inexistente';
         deferred.reject(errorMessage);
-      }else{
-        var passwordIsValid = user.validatePassword(userPassword);
-      }         
-
-      if(passwordIsValid){
-        deferred.resolve();
 
       }else{
-        var errorMessage = 'Senha inválida';
-        deferred.reject(errorMessage);
+        //Valida a senha
+        user.validatePassword(candidatePassword)
+        .then(function(isValid){
+          if(isValid){
 
-      }
+            deferred.resolve();
+
+          }else{
+
+            errorMessage = 'Senha inválida';
+            deferred.reject(errorMessage);
+
+          }
+        })
+        .catch(function(error){
+
+          errorMessage = 'Erro na validação de senha';
+          deferred.reject(errorMessage);
+          
+        });
+      };
     });
 
     return deferred.promise;
