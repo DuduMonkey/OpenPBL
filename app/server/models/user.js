687e6a040b7ca6524f788fc247d584988240acbd
DuduArnold <eduardo.arnold@outlook.com>
2015-04-09 23:47:22 -0300

    fix(Hash de senha): Corrige o problema causado pela biblioteca no hash de senhas

--- app/server/models/user.js
+++ app/server/models/user.js
@@ -3,6 +3,9 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var bcrypt = require('bcrypt-then');
+
+
 var UserSchema = new Schema({
   name: String,
   role: {type: Number, min: 1, max: 2},
@@ -10,10 +13,21 @@ var UserSchema = new Schema({
   password: String
 });
 
-//password encrypt
-UserSchema.methods.saveHashPassword = function(password) {
+//Middlaware for hashing password before save on database
+UserSchema.pre('save', function(next){
+  var user = this;
 
-};
+  if(!user.isModified('password')) return next();
+
+  bcrypt.hash(user.password, 8)
+    .then(function(hash){
+      user.password = hash;
+      next();
+    })
+    .catch(function(error){
+      return err;
+    });
+});
 
 //password decrypt and compare
 UserSchema.methods.validatePassword = function(password){
