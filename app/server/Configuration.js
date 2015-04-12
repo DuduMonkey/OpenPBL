0c01e0ae0fde6d3260aff7f11a0e9e3e58097707
DuduArnold <eduardo.arnold@outlook.com>
2015-04-12 02:20:05 -0300

    fix(Servidor): Refatora o código aplicando as regras do JSHint

--- app/server/Configuration.js
+++ app/server/Configuration.js
@@ -1,15 +1,18 @@
+/*global process, module*/
+'use strict';
+
+// Declare express app as global
 var globalApplication;
 
-module.exports = function(express,app,path,mongoose,cookieParser,bodyParser){
+// Database connection address.
+var databaseURI = process.env.CONN_STRING;
+
+// Initalize all the express server configurations.
+module.exports = function(express, app ,path ,mongoose ,cookieParser ,bodyParser ) {
 
   //set the global app
   globalApplication = app;
 
-  // Environment & Configuration Variables
-  var environment = process.env.NODE_ENV;
-  var databaseURI = process.env.CONN_STRING;
-
-  //Configure context
   configurePublicPath(express,path);
 
   configureCookieParser(cookieParser);
@@ -21,25 +24,32 @@ module.exports = function(express,app,path,mongoose,cookieParser,bodyParser){
 };
 
 
-var configurePublicPath = function(express,path){
+/**
+  Private Fields:
+
+    configurePublicPath   =>  Set the default public acessible path as 'app/public'
+
+    configureCookieParser =>  Get all the cookies baby (͡°͜ʖ͡°)
+
+    configureBodyParser   =>  Enable parsing html forms and URL encoded JSON
+
+    configureDatabase     =>  Gear UP the database mongoose communication, 
+                              by default, 'mongodb://' is already set.                          
+*/
+var configurePublicPath = function(express, path) {
   globalApplication.use(express.static(path.resolve('app/public')));
 };
 
-var configureCookieParser = function(cookieParser){
-  globalApplication.use(cookieParser()); //read cookies (required for auth)
+var configureCookieParser = function(cookieParser) {
+  globalApplication.use(cookieParser());
 };
 
-var configureBodyParser = function(bodyParser){
-  globalApplication.use(bodyParser.json()); //get information from html forms
-  globalApplication.use(bodyParser.urlencoded({
-    extended: true
-  }));
-
+var configureBodyParser = function(bodyParser) {
+  globalApplication.use(bodyParser.json());
+  globalApplication.use(bodyParser.urlencoded({ extended: true }));
 };
 
-var configureDataBase = function(engine, url){
-  // Database Connection
-  var connectionString = "mongodb://" + url;
-
-  engine.connect(connectionString);
-}
+var configureDataBase = function(databasEngine, url) {
+  var connectionString = 'mongodb://' + url;
+  databasEngine.connect(connectionString);
+};
