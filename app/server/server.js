626562a92bc3de5bc8518da8153d402e279bdbf1
DuduArnold <eduardo.arnold@outlook.com>
2015-04-12 02:20:05 -0300

    fix(Servidor): Refatora o código aplicando as regras do JSHint

--- app/server/server.js
+++ app/server/server.js
@@ -1,6 +1,7 @@
+/*global require, process*/
 'use strict';
 
-// Set Up
+// Basic Modules in use
 var express = require('express');
 var app = express();
 var path = require('path');
@@ -10,14 +11,13 @@ var mongoose = require('mongoose');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-
+// Start the configurations
 require('./Configuration')(express,app,path,mongoose,cookieParser,bodyParser);
 
-//Initialize the main Router
+// Set the main router
 var router = express.Router();
 require('./API/APIGateway.js')(app, path, router);
 
 // Server Initialization
 var port = process.env.PORT || 9000;
-
 app.listen(port);
