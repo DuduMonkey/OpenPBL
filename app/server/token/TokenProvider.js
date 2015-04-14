b8077e7b01ff33a2703e0d24dd186ba4a27e3724
Eduardo Arnold <eduardo.arnold@outlook.com>
2015-04-14 14:27:00 -0300

    style(Refatoração): Correção da estrutura usando singleton para as classes de Token, correção de estilo

--- app/server/token/TokenProvider.js
+++ app/server/token/TokenProvider.js
@@ -3,40 +3,33 @@
 
 // Modules in use
 var Token = require('./Token');
-var Q = require ('q');
-
-// Constructor
-function TokenProvider() {
-}
+var Q = require('q');
 
 /**
   Create a token using the user email payload
 
   Return the promise as resolved if token was generated (save)
 */
-TokenProvider.prototype.createToken = function(userMail) {
-
+var createToken = function (userMail) {
   var deferred = Q.defer();
 
   var newToken = new Token({
     email: userMail,
     token: userMail,
   });
-  
-  newToken.save(function(error, data) {
+
+  newToken.save(function (error, data) {
     if (error) {
       var errorMessage = { message: 'Erro na criação ao gerar token de sessão' };
-      
       deferred.reject(errorMessage);
     }
-      
     deferred.resolve(data.token);
   });
 
   return deferred.promise;
 };
 
-
-
-// Export the module as TokenProvider
-module.exports = TokenProvider;
+// Export the module as singleton the token provider Type
+module.exports = {
+  createToken : createToken
+};
