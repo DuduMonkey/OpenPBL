70584d02cf7a35da4b45b46008ce933cb988f32d
Eduardo Arnold <eduardo.arnold@outlook.com>
2015-04-16 13:21:08 -0300

    style(Refatoração): Corrige as constantes e aplica o Hint

--- app/server/models/constants/user_role.js
+++ app/server/models/constants/user_role.js
@@ -1,7 +1,14 @@
-const TEACHER = 1;
-const STUDENT = 2;
+/*global module*/
+'use strict';
+
+/**
+  1 = The user are a Teacher
+  2 = The user are a Student
+*/
+var __TEACHER = 1;
+var __STUDENT = 2;
 
 module.exports = {
-  TEACHER: TEACHER,
-  STUDENT: STUDENT
+  TEACHER: __TEACHER,
+  STUDENT: __STUDENT
 };
