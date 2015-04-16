a165add4e0b5fb30f10041248768dca68fbb1c49
Eduardo Arnold <eduardo.arnold@outlook.com>
2015-04-16 13:48:41 -0300

    style(Refatoração): Aplica o Hint ao código e altera o header para X-Pbl-Token

--- app/server/api/APIProxy.js
+++ app/server/api/APIProxy.js
@@ -1,54 +1,66 @@
+/*global module, require, process*/
+'use strict';
+
+// Modules in use
 var TokenValidator = require('../token/TokenValidator');
 
-module.exports = function(router){
+//Token location on header (You can send as X-Pbl-Token)
+var __TOKEN_HEADER = 'x-pbl-token';
 
-  router.use(function(req, res, next) {
-
-    var url = req.url;
-    var baseUrl = req.baseUrl;
-
-
-    if(pathNeedsAuthentication(url,baseUrl)){
-
-      var tokenField = 'x-pbl-user-auth';
-      var headerToken = req.headers[tokenField];
-
-       TokenValidator.validateToken(headerToken)
-         .then(function(){
-
-            next();
-
-          })
-          .catch(function(){
-
-            res.status(401).send({message: 'Acesso não autorizado'});
-
-          });
-
-    }else{
-
-      next();
-
-    };
-
-  });
-};
-
-var pathNeedsAuthentication = function(url, baseUrl){
+/**
+  Validate if the requested path needs authentication
+  Requested path is free from authentication case:
+      Base url are doesnt have exists (/api)
+      Url are listed as free from authentication
+  Otherwise, it needs authentication
+*/
+var pathNeedsAuthentication = function (url, baseUrl) {
   var freeFromAuthenticationPaths = [
     '/signup',
     '/login'
   ];
 
-  if(!baseUrl){
+  if (!baseUrl) {
     return false;
-  }else{
-    var freeFromAuthentication = (freeFromAuthenticationPaths.indexOf(url) > -1);
-    if(freeFromAuthentication){
-      return false;
-    }else{
-      return true;
-    }
   }
+  var freeFromAuthentication = (freeFromAuthenticationPaths.indexOf(url) > -1);
+  if (freeFromAuthentication) {
+    return false;
+  }
+  return true;
+};
 
+/**
+  Validate the token candidate, if the token are invalid
+  send HTTP status 401
+*/
+var validateToken = function (tokenCandidate, next, res) {
+  TokenValidator.validateToken(tokenCandidate)
+    .then(function () {
+      next();
+    })
+    .catch(function () {
+      res.status(401).send({ message: 'Acesso não autorizado' });
+    });
+};
+
+/**
+  "Where the magic happens"
+  All the requests pass throught this function
+*/
+var routeMiddleware = function (req, res, next) {
+  var url = req.url;
+  var baseUrl = req.baseUrl;
+
+  if (pathNeedsAuthentication(url, baseUrl)) {
+    var headerToken = req.headers[__TOKEN_HEADER];
+    validateToken(headerToken, next, res);
+  } else {
+    next();
+  }
+};
+
+//Export the runnable module APIProxy
+module.exports = function (router) {
+  router.use(routeMiddleware);
 };
