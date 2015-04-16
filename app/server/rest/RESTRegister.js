21d4b1415a1ad7454b292835cd67f7c4d9f46cf1
Dudu <eduardo.arnold@outlook.com>
2015-04-16 09:07:34 -0300

    style(Refatoração): Refatora as classes de alto nível do registro aplicando o JSHint

--- app/server/rest/RESTRegister.js
+++ app/server/rest/RESTRegister.js
@@ -1,20 +1,26 @@
+/*global module, require, process*/
+'use strict';
+
+// Modules in use
 var registerService = require('../services/RegisterService'); //Initialize and run
-var Q = require ('q');
+var Q = require('q');
 
-exports.post = function(req,res){
-
+/**
+  Define the post action for user registration
+  Return the new user email and the success status on successful register.
+*/
+exports.post = function (req, res) {
   registerService.registerUser(req.body)
-    .then(function(newUser){
+    .then(function (newUser) {
       res.send({
         email : newUser.email,
         success: true
       });
     })
-    .catch(function(errorMessage){
+    .catch(function (errorMessage) {
       res.send({
         success: false,
         reason : errorMessage
       });
     });
-
-}
+};
