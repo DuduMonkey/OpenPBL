957d90540752fb52cf80a5770fdf6fa35ac99eb6
Dudu <eduardo.arnold@outlook.com>
2015-04-16 09:08:12 -0300

    style(Refatoração): Refatora a model de atividade aplicando JSHint

--- app/server/models/Activity.js
+++ app/server/models/Activity.js
@@ -1,11 +1,17 @@
-var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+/*global module, require, process*/
+'use strict';
+
+// Modules in use
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
 
 var ActivitySchema = new Schema({
-  id: {Type: Number, min: 1, max, 10},
+  id: {Type: Number, min: 1, max: 10},
   name: String,
-  state: {Type: Number, min: 1, max, 1},
-  story: String
+  state: {Type: Number, min: 1, max: 1},
+  story: String,
+  startDate: {type: Date, default: Date.now}
 });
 
 module.exports = mongoose.model('Activity', ActivitySchema);
