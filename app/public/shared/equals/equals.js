01c673bd18745b1a6d1cb68109ce3ff5b9c48f65
Jeison Spaniol <mr.jasonn@gmail.com>
2015-04-18 20:07:33 -0300

    style(equals): remove comentários e log

--- app/public/shared/equals/equals.js
+++ app/public/shared/equals/equals.js
@@ -4,32 +4,26 @@
   angular.module('openpbl.directives')
     .directive('pblEquals', function () {
       return {
-        restrict: 'A', // only activate on element attribute
-        require: '?ngModel', // get a hold of NgModelController
+        restrict: 'A',
+        require: '?ngModel',
         link: function(scope, element, attrs, ngModel) {
           if (!ngModel) {
-            console.log('meh')
-            return; // do nothing if no ng-model
+            return;
           }
 
-          // watch own value and re-validate on change
           scope.$watch(attrs.ngModel, function() {
             validate();
           });
 
-          // observe the other value and re-validate on change
           attrs.$observe('pbl-equals', function () {
             validate();
           });
 
           var validate = function () {
-            // values
             var value1 = ngModel.$viewValue
             , value2 = attrs.pblEquals
             , isValid = !!value1 && !!value2 && value1 === value2;
 
-            console.log('validate', isValid, value1, value2);
-            // set validity
             ngModel.$setValidity('equals', isValid);
           };
         }
