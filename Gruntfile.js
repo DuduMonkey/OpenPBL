611c7534437e24f46cfafdbf152862c523276990
Jeison Spaniol <mr.jasonn@gmail.com>
2015-04-18 12:50:27 -0300

    chore(Gruntfile): remover dependencia desnecessária

--- Gruntfile.js
+++ Gruntfile.js
@@ -137,7 +137,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-express-server');
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-plato');
