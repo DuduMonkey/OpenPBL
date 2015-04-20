1eb5a4deee1fd254dcaa374cee4c05e1baa6617c
eduardo.arnold@outlook.com <Wyo9030*>
2015-04-20 09:09:29 -0300

    feat(Atividade): Insere a a exceção Token não econtrada

--- app/server/shared/Exceptions.js
+++ app/server/shared/Exceptions.js
@@ -7,13 +7,17 @@
   };
 
   module.exports = {
+    //Token Errors
     INVALID_TOKEN : declareNewError('Usuário não autenticado'),
     TOKEN_CREATION_ERROR: declareNewError('Erro na geração de uma nova Token'),
     TOKEN_HASHING_ERROR: declareNewError('Erro na geração do hash de Token'),
+    TOKEN_FIND_ERROR: declareNewError('Erro na do documento da token'),
+    //User Errors
     USER_FIND_ERROR: declareNewError('Erro na busca de usuário'),
     USER_ALREADY_EXISTS: declareNewError('Email já cadastrado'),
     USER_PERSISTENCE_ERROR: declareNewError('Erro na persistencia dos dados de usuário'),
     USER_NOT_FIND: declareNewError('Usuário não encontrado'),
+    //PASSWORD ERROR
     PASSWORD_HASHING_ERROR: declareNewError('Erro no hash de senha'),
     PASSWORD_NOT_MATCH: declareNewError('A senha não confere')
   };
