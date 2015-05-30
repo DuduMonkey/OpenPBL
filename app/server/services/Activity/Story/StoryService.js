dfeeed2521ae0cf16696cd5a559b870cd0e1a667
eduardo.arnold@outlook.com <Wyo9030*>
2015-05-30 19:07:38 -0300

    feat(Activity Story): Define o fluxo do serviço de atualização do problema da atividade

--- app/server/services/Activity/Story/StoryService.js
+++ app/server/services/Activity/Story/StoryService.js
@@ -4,38 +4,43 @@
   //Modules in use
   var Story = require('../../../models/Story');
   var Activity = require('../../../models/Activity');
-  var activityState = require('../../../models/constants/activity_state');
   var Message = require('../../../shared/MessageResource');
   var Q = require('q');
 
   //Specifications in use
   var ActivitySpec = require('../Specification/ActivitySpec');
 
-  var updateActivityStory = function (activityId, storyViewData, storyId) {
+  /**
+    Update an activity story based on story data received and story uid
+  **/
+  var updateActivityStory = function (storyViewData, storyId) {
     var deferred = Q.defer();
 
     var queryUpdateStory = {
-      $set: { 
+      $set: {
         description: storyViewData.description,
         helpfulMaterials: storyViewData.helpfulMaterials,
+        externalLinks: storyViewData.externalLinks
       }
     };
 
     Story.updateStory(storyId, queryUpdateStory)
-      .then(function (story) {
-        deferred.resolve(story);
+      .then(function () {
+        deferred.resolve();
       })
       .catch(function (error) {
         deferred.reject(error);
       });
 
     return deferred.promise;
-  }
+  };
 
+  /**
+    Save an activity story on activity received
+  **/
   var saveActivityStory = function (activityId, storyViewData) {
     var deferred = Q.defer();
 
-    //new Story data
     var storyData = {
       activityId: activityId,
       description: storyViewData.description,
@@ -43,57 +48,57 @@
       externalLinks: storyViewData.externalLinks
     };
 
-    Story.saveNewStory(story)
+    Story.saveNewStory(storyData)
       .then(function (newStory) {
         return Activity.updateActivity(
           activityId,
           {
-            story: newStory._id 
+            story: newStory._id
           }
         );
       })
       .then(function () {
-        var responseBag = persistedStoryResponseBag();
-        deferred.resolve(responseBag);
+        deferred.resolve();
       })
       .catch(function (error) {
         deferred.reject(error);
       });
 
     return deferred.promise;
-  }
+  };
 
-    /**
-    Set an activity story and update the activity status
+  /**
+    Insert activity story using strategy to find if is update or insert
   **/
   var insertActivityStory = function (activityId, storyViewData) {
     var deferred = Q.defer();
 
-    Activity.queryInActivities({
-            select: '-_id story',
-            where: ['_id'],
-            conditions: [activityId],
-            join: [
-              {
-                path: 'story',
-                select: '_id'
-              }
-            ]
-      })
+    var queryFindActivity = {
+      select: '-_id story',
+      where: ['_id'],
+      conditions: [activityId],
+      join: [
+        {
+          path: 'story',
+          select: '_id'
+        }
+      ]
+    };
+
+    Activity.queryInActivities(queryFindActivity)
       .then(function (activities) {
         var selectedActivity = activities[0];
         if (ActivitySpec.ActivityHasStory().isSatisfiedBy(selectedActivity)) {
           return updateActivityStory(activityId, storyViewData, selectedActivity.story._id);
         }
-        console.log('nao tem problema');
+        return saveActivityStory(activityId, storyViewData);
       })
-      .then(function (story) {
-        console.log(story);
+      .then(function () {
         deferred.resolve({ message: Message.SUCCESS_SAVING_STORY });
       })
       .catch(function (error) {
         deferred.reject(error);
-      })
+      });
 
     return deferred.promise;
   };
