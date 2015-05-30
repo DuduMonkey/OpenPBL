12d8ac4b7753102106230eb19fb9bf78c1e8ad94
eduardo.arnold@outlook.com <Wyo9030*>
2015-05-30 14:02:43 -0300

    feat(Atividade Listar): Implementa as estrategias de listagem para professor e aluno

--- app/server/services/Activity/ActivityListStrategy.js
+++ app/server/services/Activity/ActivityListStrategy.js
@@ -2,28 +2,86 @@
   'use strict';
 
   var Q = require('q');
+  var Role = require('../../models/constants/user_role');
+  var Activity = require('../../models/Activity');
 
-  function ActivityListStrategy () {
+  function ActivityListStrategy() {
 
+    /**
+      Busca as atividades do professor (criador) da atividade
+    **/
     var getTeacherActivities = function (userId) {
       var deferred = Q.defer();
-      //find all activities from user and populate participants
+
+      //Activities select name, story, created, participants, status where _creator = user._id
+      Activity.queryInActivities({
+        select: '_id name story created participants status',
+        where: ['_creator'],
+        conditions: [userId],
+        join: [
+          {
+            path: 'participants',
+            select: '_id name'
+          },
+          {
+            path: 'story',
+            select: '-_id description'
+          }
+        ]
+      })
+        .then(function (activities) {
+          deferred.resolve(activities);
+        })
+        .catch(function (error) {
+          deferred.reject(error);
+        });
+
       return deferred.promise;
     };
 
+    /**
+      Busca as atividades do aluno (participante) da atividade
+    **/
     var getStudentActivities = function (userId) {
       var deferred = Q.defer();
-      //find all activities where user in and populate participants
+
+      //Activities select name, story, created, participants, status where participants contains user._id
+      Activity.queryInActivities({
+        select: '_id name story created participants status',
+        where: ['participants'],
+        conditions: [userId],
+        join: [
+          {
+            path: 'participants',
+            select: '_id name'
+          },
+          {
+            path: 'story',
+            select: '-_id description'
+          }
+        ]
+      })
+        .then(function (activities) {
+          deferred.resolve(activities);
+        })
+        .catch(function (error) {
+          deferred.reject(error);
+        });
+
       return deferred.promise;
     };
 
+    // Strategy Method
     var getUserActivities = function (user) {
       var deferred = Q.defer();
-      //switch
-        //case teacher
-          //return getTeacherActivities(user._id)
-        //case student
-          //return getStudentActivities(user._id)
+
+      switch (user.role) {
+      case Role.TEACHER.value:
+        return getTeacherActivities(user._id);
+      case Role.STUDENT.value:
+        return getStudentActivities(user._id);
+      }
+
       return deferred.promise;
     };
 
