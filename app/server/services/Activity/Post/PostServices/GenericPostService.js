d1be84c3ee3a81f4944a051befa0c4168972428c
eduardo.arnold@outlook.com <Wyo9030*>
2015-06-07 01:34:37 -0300

    refactor(Post): Cria um módulo genérico para execuções na base de posts

---
+++ app/server/services/Activity/Post/PostServices/GenericPostService.js
@@ -0,0 +1,24 @@
+(function () {
+  'use strict';
+
+  var Post = require('../../../../models/Post');
+  var Message = require('../../../../shared/MessageResource');
+  var Q = require('q');
+
+  var insertPostData = function (postData) {
+    var deferred = Q.defer();
+    Post.saveNewPost(postData)
+      .then(function () {
+        deferred.resolve();
+      })
+      .catch(function (error) {
+        deferred.reject(error);
+      });
+    return deferred.promise;
+  };
+
+  module.exports = {
+    insertPostData: insertPostData
+  };
+
+}());
