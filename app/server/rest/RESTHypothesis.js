d1584e07d24d8df7c5042a7fa8edf22488a3e355
eduardo.arnold@outlook.com <Wyo9030*>
2015-06-07 01:39:01 -0300

    refactor(Post Rest): Corrige as facades das rotas

--- app/server/rest/RESTHypothesis.js
+++ app/server/rest/RESTHypothesis.js
@@ -3,9 +3,10 @@
   'use strict';
 
   // Modules in use
-  var postService = require('../services/Activity/Fact/PostService');
+  var postHandler = require('../services/Activity/Post/PostHandler');
   var TYPE = require('../models/constants/post_type');
   var _TOKEN_HEADER = 'x-pbl-token';
+  
   /**
     Define the post action for user authentication
     Return HTTP status 200 and the session token on valid authentication
@@ -14,7 +15,7 @@
     var headerToken = req.headers[_TOKEN_HEADER];
     var activityId = req.params.id;
 
-    postService.insertNewPost(headerToken, activityId, req.body, TYPE.HYPOTHESIS)
+    postHandler.usingServiceOfType(TYPE.HYPOTHESIS).insertNewPost(headerToken, activityId, req.body)
       .then(function (responseBag) {
         res.status(200).send(responseBag);
       })
